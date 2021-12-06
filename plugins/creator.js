360cfbe52002c30346cf809801a3d54764205aca
tahaluindo <94370774+tahaluindo@users.noreply.github.com>
2021-12-06 22:57:21 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '6282324892737', 'Fokus Dot Id', m)
-  this.sendContact(m.chat, '6282324892737', 'Fokus Dot Id', m)
+  // F this.sendContact(m.chat, '62895369318181', 'AhmadChenDev', m)
+  this.sendContact(m.chat, '62895369318181', 'Ahmad Chen Id', m)
   conn.reply(m.chat, `Nih OwnerKu Jangan dispam yah >_<`, m)
   
 }
